25e684a5f90b9e26f5b3614c0a07704fc9358f55
Ikhsan S <93766033+ikhsan3adi@users.noreply.github.com>
2023-06-08 14:50:00 +0700

    data validation implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,9 @@
+const ClientError = require('../../exceptions/ClientError');
+
 class NotesHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,7 +14,9 @@ class NotesHandler {
 
   postNoteHandler(request, h) {
     try {
-      const { title = 'untitled', tags, body } = request.payload;
+      this._validator.validateNotePayload(request.payload);
+
+      const { title = 'untitled', body, tags } = request.payload;
 
       const noteId = this._service.addNote({ title, body, tags });
 
@@ -25,11 +30,22 @@ class NotesHandler {
       response.code(201);
       return response;
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message,
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
-      response.code(400);
+      response.code(500);
+      console.error(error);
       return response;
     }
   }
@@ -57,17 +73,30 @@ class NotesHandler {
         },
       };
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message,
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
-      response.code(404);
+      response.code(500);
+      console.error(error);
       return response;
     }
   }
 
   putNoteByIdHandler(request, h) {
     try {
+      this._validator.validateNotePayload(request.payload);
+
       const { id } = request.params;
 
       this._service.editNoteById(id, request.payload);
@@ -77,11 +106,22 @@ class NotesHandler {
         message: 'Catatan berhasil diperbarui',
       };
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message,
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
-      response.code(404);
+      response.code(500);
+      console.error(error);
       return response;
     }
   }
@@ -97,11 +137,22 @@ class NotesHandler {
         message: 'Catatan berhasil dihapus',
       };
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message,
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
       });
-      response.code(404);
+      response.code(500);
+      console.error(error);
       return response;
     }
   }
--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,6 @@
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -18,7 +20,7 @@ class NotesService {
 
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
-    if (!isSuccess) throw new Error('Catatan gagal ditambahkan');
+    if (!isSuccess) throw new InvariantError('Catatan gagal ditambahkan');
 
     return id;
   }
@@ -30,7 +32,7 @@ class NotesService {
   getNoteById(id) {
     const note = this._notes.filter((n) => n.id === id)[0];
 
-    if (!note) throw new Error('Catatan tidak ditemukan');
+    if (!note) throw new NotFoundError('Catatan tidak ditemukan');
 
     return note;
   }
@@ -38,7 +40,7 @@ class NotesService {
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((n) => n.id === id);
 
-    if (index === -1) throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    if (index === -1) throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
 
     const updatedAt = new Date().toISOString();
 
@@ -54,7 +56,7 @@ class NotesService {
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
 
-    if (index === -1) throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+    if (index === -1) throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
 
     this._notes.splice(index, 1);
   }
